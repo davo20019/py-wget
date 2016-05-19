139ab52f587a6372323c539f12e98ee4570c2303
David Loor <davo_20034@yahoo.com>
2016-05-18 21:31:34 -0500

    General parameters.

--- wget-python.py
+++ wget-python.py
@@ -9,7 +9,7 @@ db = MySQLdb.connect(host = "127.0.0.1", user = "username", passwd = "password",
 cursor = db.cursor()
 
 # Use all the SQL you like
-cursor.execute("SELECT filename FROM file_managed")
+cursor.execute("SELECT column FROM table")
 
 # get current working directory
 path = os.getcwd()
@@ -23,7 +23,7 @@ for row in cursor.fetchall():
     if os.path.isfile(file):
       print "file does exist at this time: " + file
     else:
-      url = "https://communityofhopedc.org/sites/default/files/" + file_name
+      url = "https://domain.com/public/folder/" + file_name
       urllib.urlretrieve(url, filename = file_name)
       print "New file downloaded: " + file_name
 
